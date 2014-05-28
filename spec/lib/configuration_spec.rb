65e72627441dd556f21b79e18d19deaa8dc9d7d4
Artur Pañach <arturictus@gmail.com>
2014-05-28 11:10:05 +0200

    ignore rubymine folder

--- spec/lib/configuration_spec.rb
+++ spec/lib/configuration_spec.rb
@@ -1,19 +1,1 @@
-require "spec_helper"
-
-module Coins
-  describe Configuration do
-    describe "#tax_rate" do
-      it "default value is 6" do
-        Configuration.new.tax_rate = 16
-      end
-    end
-
-    describe "#tax_rate=" do
-      it "can set value" do
-        config = Configuration.new
-        config.tax_rate = 7
-        expect(config.tax_rate).to eq(7)
-      end
-    end
-  end
-end
+require "spec_helper"module Coins  describe Configuration do    describe "#tax_rate" do      it "default value is 6" do        Configuration.new.tax_rate = 16      end    end    describe "#tax_rate=" do      it "can set value" do        config = Configuration.new        config.tax_rate = 7        expect(config.tax_rate).to eq(7)      end    end  endend

df9263341336cc0e8914c9de316cbaaa5b220ac7
Artur Pañach <arturictus@gmail.com>
2014-06-11 18:55:57 +0200

    - added new method returning TaxCalculations object so new data it's accesible: tax_amount and tax.

--- lib/coins/tax_calculations.rb
+++ lib/coins/tax_calculations.rb
@@ -13,6 +13,12 @@ module Coins
       number.calculate_tax
       number_with_precision(number.taxed, :precision => 2)
     end
+
+    def tax_it_extended(price)
+    	number = Taxable.new(price)
+      number.calculate_tax
+      number
+    end
   end
 
 end
